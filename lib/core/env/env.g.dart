1ee7f96582d9a1a83c1f4ce83a230a896283724c
Mohamed Maher <139143509+mohamedmaher-dev@users.noreply.github.com>
2025-03-18 01:26:51 -0700

    done

---
+++ lib/core/env/env.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'env.dart';
+
+// **************************************************************************
+// EnviedGenerator
+// **************************************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+final class _Env {
+  static const String apiBaseUrl = 'https://commuter.onrender.com/api/v1/';
+
+  static const String socketIoBaseUrl =
+      'https://commuter-server-socket-io.onrender.com';
+
+  static const String apiChatBaseUrl =
+      'https://commuter-server-socket-io.onrender.com/';
+
+  static const String apiNotifiBaseUrl =
+      'https://commuter-server-socket-io.onrender.com/notfy/';
+
+  static const String routesApiKey = 'AIzaSyDb5e73IeH3l1BoLSbPqavbgtSXtiqLGgs';
+
+  static const String placesApiKey = 'AIzaSyANjxsXgg4zOWokfsNqWojVQlXLMwwymjU';
+}
